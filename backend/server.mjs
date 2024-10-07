fd2a8235e5ac40ec57f3c2d7cc60af2bc38facd9
Mickael Joly <mickaeljoly@MacBook-Pro-de-Mickael.local>
2024-10-07 10:42:53 +0200

    add: cors and first request

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,15 @@
 import https from 'https';
 import fs from "fs";
 import express, { response } from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+dotenv.config();
 
 const privateKey = fs.readFileSync('./security/localhost-key.pem', 'utf8');
 const certificate = fs.readFileSync('./security/localhost.pem', 'utf8');
@@ -10,4 +18,31 @@ const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(3000, () => {
     console.log('serveur https en écoute sur le port 3000');
+    console.log(process.env.TOKEN);
+});
+
+const fetchCurrentMovie = async () => {
+    const url = 'https://api.themoviedb.org/3/movie/changes?page=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjY1OTViNjZkMzM4ODBlYzg4Y2E0ODI3ZDY2NzI0OSIsIm5iZiI6MTcyODIyMjk4OS4wNzA2MTksInN1YiI6IjY2ZmZjN2ZiYzlhMTBkNDZlYTdjYzk4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S1OiI9QTGmQqohd81rQ2RuW2_UsRk9q0gHKo9kclf58'
+      }
+    };
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        console.log('error:' + error)
+    }
+    
+}
+
+
+app.get('/movies', async (req, res) => {
+    const datalist = await fetchCurrentMovie();
+    console.log(datalist);
+    res.json("oui");
 });
