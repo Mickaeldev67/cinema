6709afea19b4cb4ab8be5d127663426b835c3524
Mickael Joly <mickaeljoly@MacBook-Pro-de-Mickael.local>
2024-10-11 10:07:50 +0200

    add: image in front

--- cinemalib/src/App.jsx
+++ cinemalib/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+
+  const fetchingData = async () => {
+    try {
+      const response = await axios.get('https://localhost:3000/movies');
+      console.log(response.data);
+      setMovies(response.data);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchingData();
+  },[]);
+  
 
   return (
     <>
-      <p className="bg-gray-300 text-gray-800">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="bg-gray-300 text-gray-800">
+        Cinémathèque
+      </h1>
+      <div className='flex'>
+      {
+        movies.map(({id, title, poster_path}) => (
+          <>
+            <div className='flex flex-col items-center'>
+              <img className="h-60 w-60" src={poster_path === null 
+              ? 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'  // Image par défaut si poster_path est null
+              : `https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path}`}  // Image normale  
+              alt={title} />
+              <p key={id}>{title}</p>
+            </div>
+          </>
+        ))
+      }
+      </div>
     </>
   )
 }
