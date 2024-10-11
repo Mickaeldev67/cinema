1adaf9b18660b531a6b142ab436442bcc35e0eae
Mickael Joly <mickaeljoly@MacBook-Pro-de-Mickael.local>
2024-10-11 12:59:45 +0200

    add: composant home and moviesdetails.jsx

--- cinemalib/src/App.jsx
+++ cinemalib/src/App.jsx
@@ -1,46 +1,14 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Home from './components/Home';
 
 function App() {
-  const [movies, setMovies] = useState([])
-
-  const fetchingData = async () => {
-    try {
-      const response = await axios.get('https://localhost:3000/movies');
-      console.log(response.data);
-      setMovies(response.data);
-    } catch(error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchingData();
-  },[]);
-  
-
-  return (
+  return(
     <>
-      <h1 className="bg-gray-300 text-gray-800">
-        Cinémathèque
-      </h1>
-      <div className='flex'>
-      {
-        movies.map(({id, title, poster_path}) => (
-          <>
-            <div className='flex flex-col items-center'>
-              <img className="h-60 w-60" src={poster_path === null 
-              ? 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'  // Image par défaut si poster_path est null
-              : `https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path}`}  // Image normale  
-              alt={title} />
-              <p key={id}>{title}</p>
-            </div>
-          </>
-        ))
-      }
-      </div>
+      <Home />
     </>
   )
+  
 }
 
-export default App
+export default App;
